939ea51a20378c29c82a7b5dea796677e153c35f
WinLeaf1239 <winterleaf1239@tutamail.com>
2025-10-21 23:56:07 +0300

    1234

--- plot.py
+++ plot.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.linspace(0,10,20,30,40,50)
-y = x **2 + x*2 +8
-plt.plot(x, y)
-plt.show()
+x = np.linspace(0,30,100) #область определения и мн-во точек
+y = x **2 + x*2 +8 #Функция
+plt.plot(x, y) #График
+plt.show() #Вывод
